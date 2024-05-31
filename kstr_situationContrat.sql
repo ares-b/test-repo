13bfaf5ecf4213285d46dd6792a77b4a38f6ebbc
Arès <49230966+ares-b@users.noreply.github.com>
2024-05-31 11:35:33 +0200

    Update kstr_situationContrat.sql

--- kstr_situationContrat.sql
+++ kstr_situationContrat.sql
@@ -1,14 +1,14 @@
 SELECT
-    SOURCE_TABLE.POL_POSITION_ID                        AS 'ID_SITUATION',
-    SOURCE_TABLE.POLICY_ID                              AS 'ID_CONTRAT',
-    SOURCE_TABLE.STATUS                                 AS 'STATUT',
-    SOURCE_TABLE.CREATION_DATE                          AS 'DATE_CREATION',
-    SOURCE_TABLE.EFFECT_DATE                            AS 'DATE_EFFET',
-    SOURCE_TABLE.REAL_NET_AMOUNT                        AS 'VAC_NETTE_REAL',
-    SOURCE_TABLE.EFFECTIVESTARTDATE                     AS 'EFFECTIVESTARTDATE',
-    SOURCE_TABLE.EFFECTIVEENDDATE                       AS 'EFFECTIVEENDDATE',
-    SYSDATE                                             AS 'LASTMODIFIED',
-    SYSDATE                                             AS 'CREATEDDATE'
+    SOURCE_TABLE.POL_POSITION_ID                        AS ID_SITUATION,
+    SOURCE_TABLE.POLICY_ID                              AS ID_CONTRAT,
+    SOURCE_TABLE.STATUS                                 AS STATUT,
+    SOURCE_TABLE.CREATION_DATE                          AS DATE_CREATION,
+    SOURCE_TABLE.EFFECT_DATE                            AS DATE_EFFET,
+    SOURCE_TABLE.REAL_NET_AMOUNT                        AS VAC_NETTE_REAL,
+    SOURCE_TABLE.EFFECTIVESTARTDATE                     AS EFFECTIVESTARTDATE,
+    SOURCE_TABLE.EFFECTIVEENDDATE                       AS EFFECTIVEENDDATE,
+    SYSDATE                                             AS LASTMODIFIED,
+    SYSDATE                                             AS CREATEDDATE
 FROM
     OWN_24456_ODS.RF_KVIC_CRE_CRE_POSITIONS SOURCE_TABLE
 WHERE
@@ -28,14 +28,14 @@ WHERE
         WHERE
             SOURCE_TABLE.POL_POSITION_ID = TARGET_TABLE.ID_SITUATION
             AND
-            SOURCE_TABLE.POLICY_ID = TARGET_TABLE.ID_CONTRAT
-            AND
             SOURCE_TABLE.LASTMODIFIED >= (
                 SELECT
                     DAT_VAL
                 FROM
                     parametrage
             )
+            AND
+            SOURCE_TABLE.POLICY_ID = TARGET_TABLE.ID_CONTRAT
             AND
             SOURCE_TABLE.STATUS = TARGET_TABLE.STATUT
             AND
